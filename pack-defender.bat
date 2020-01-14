2a3ad880f4d19d298a0dcd630c5ee15c6b70b0c8
LiCybora <40160522+LiCybora@users.noreply.github.com>
2020-01-15 01:17:14 +0800

    Delete unused .bat

--- pack-defender.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-NODE . --chromium --defender --pack
-NODE . --chromium --defender --pro --pack
-
-REM NODE . --firefox --defender --pro --pack
-REM NODE . --edge --defender --pro --pack
-
-PAUSE
